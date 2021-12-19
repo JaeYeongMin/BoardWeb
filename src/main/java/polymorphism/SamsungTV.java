5cf433d73bb95c4df794ceaaeb28f2010da854ab
jymin <mjydb@gmail.com>
2021-12-19 18:30:48 +0900

    테레비 이름 수정했음ㅎㅎ

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -3,17 +3,17 @@ package polymorphism;
 public class SamsungTV implements TV{
     private Speaker speaker;
     private int price;
-    
+
     public SamsungTV(){
-        System.out.println("===> SamsungTV(1) 객체 생성");
+        System.out.println("===> 삼성티비(1) 객체 생성");
     }
-    
+
     public SamsungTV(Speaker speaker){
-        System.out.println("===> SamsungTV(2) 객체 생성");
+        System.out.println("===> 삼성티비(2) 객체 생성");
         this.speaker = speaker;
     }
 
-   
+
     public SamsungTV(Speaker speaker, int price) {
         System.out.println("===> SamsungTV(3) 객체 생성");
         this.speaker = speaker;
@@ -30,8 +30,8 @@ public class SamsungTV implements TV{
         this.price = price;
     }
 
-    
-    
+
+
     public void powerOn(){
         System.out.println("SamsungTV --- 전원 켠다. (가격: "+price+")");
     }
@@ -43,7 +43,7 @@ public class SamsungTV implements TV{
     public void volumeUp(){
         speaker.volumeUp();
     }
-    
+
     public void volumeDown(){
         speaker.volumeDown();
     }
